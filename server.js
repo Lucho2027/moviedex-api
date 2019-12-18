3695b077b090060554fde022e360de511d836ff8
Lucho2027 <luis01.ro@gmail.com>
2019-12-17 23:10:51 -0600

    "Search by country, genre and avg_vote"

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express =require('express');
-const morgan=require('morgan')
+const morgan=require('morgan');
+const MOVIEDEX = require('./moviedex.json');
 
 const app=express()
 app.use(morgan('common'));
@@ -16,10 +17,34 @@ if (!authToken || authToken.split(' ')[1] !== apiToken){
 next()
 })
 
-function handleGetMovies(req,res){
-    res.send('Hello, Movie!')
-}
-app.get('/movie', handleGetMovies)
+
+app.get('/movie', function handleGetMovies(req,res){
+    let response = MOVIEDEX;
+
+    if(req.query.genre){
+        response = response.filter(movie =>
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+            )
+
+    }
+
+    if(req.query.country){
+        response = response.filter(movie =>
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+            )
+
+    }
+
+    if(req.query.avg_vote){
+        response = response.filter(movie =>
+            Number(movie.avg_vote) >= Number(req.query.avg_vote)
+            )
+
+    }
+    res.json(response)
+})
+
+
 
 const PORT = 8000
 
